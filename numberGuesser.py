86c86e34178a80d0c7c179afd04dd0af85ba835f
Saint-John Royce <vzhu6682@uni.sydney.edu.au>
2020-02-06 20:08:46 +0800

    added indexing

--- numberGuesser.py
+++ numberGuesser.py
@@ -4,11 +4,16 @@ number = random.randint(1,100)
 inputNumber = input('please put in your guess: ')
 inputNumber = int(inputNumber)
 
+index = 0
+
 while True:
+
+	index += 1
+
 
 	if inputNumber == number:
 
-		print('correct, the number is indeed', number)
+		print('correct, the number is indeed', number, '; took you', index, 'time(s) to solve')
 
 		break
 
